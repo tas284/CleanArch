e653fa229364f14ac9f1bc045667a2b3515ba6a5
tas284 <tiago.avila.saldanha@gmail.com>
2024-02-10 14:01:08 -0300

    Add IMemberRepository

---
+++ CleanArch.Domain/Interfaces/IMemberRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArch.Domain.Entities;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface IMemberRepository
+    {
+        Task<IEnumerable<Member>> GetAll();
+        Task<Member> GetMemberById(int id);
+        Task<Member> AddMember(Member member);
+        Task<Member> UpdateMember(Member member);
+        Task DeleteMember(int memberId);
+    }
+}
