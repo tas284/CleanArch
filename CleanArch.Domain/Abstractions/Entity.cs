0d0b908f16527f9cc066cecb8b0d2b669d9e2cb4
tas284 <tiago.avila.saldanha@gmail.com>
2024-02-10 13:18:28 -0300

    Add Domain

---
+++ CleanArch.Domain/Abstractions/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArch.Domain.Abstractions
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
---
+++ CleanArch.Domain/Entities/Member.cs
@@ -0,0 +1,11 @@
+﻿using CleanArch.Domain.Abstractions;
+
+namespace CleanArch.Domain.Entities
+{
+    public class Member : Entity
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
